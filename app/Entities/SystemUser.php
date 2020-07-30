0c9961c73d2ca4344e66d8edefbda619ec9757ea
terryyao <terry.yao.global@gmail.com>
2020-07-30 19:56:23 +0800

    insertjwt

--- app/Entities/SystemUser.php
+++ app/Entities/SystemUser.php
@@ -2,12 +2,60 @@
 
 namespace App\Entities;
 
-use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class SystemUser extends Model
+class SystemUser extends Authenticatable implements JWTSubject
 {
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'eip_member_id', 'username', 'password', 'group',
-        'main_menu_id', 'sub_menu_id', 'functions', 'is_active'
+        'eip_member_id', 'username', 'password', 'group', 'store', 'main_menu_id',
+        'sub_menu_id', 'functions', 'is_active'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        // 'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
