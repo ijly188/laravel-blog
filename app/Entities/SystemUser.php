e9348440dfb17ad4869066493dd99fda72f3da0b
terryyao <terry.yao.global@gmail.com>
2020-07-30 16:34:49 +0800

    createsystemuserseeder

--- app/Entities/SystemUser.php
+++ app/Entities/SystemUser.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SystemUser extends Model
 {
     protected $fillable = [
-        'eip_member_id', 'username', 'password', 'group', 'store',
+        'eip_member_id', 'username', 'password', 'group',
         'main_menu_id', 'sub_menu_id', 'functions', 'is_active'
     ];
 }
