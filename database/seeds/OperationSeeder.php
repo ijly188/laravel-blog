2d838a90bea485c829c120fd401ffbe0e1b6f1a4
terryyao <terry.yao.global@gmail.com>
2020-08-03 14:00:49 +0800

    create system user operation seeder data

--- database/seeds/OperationSeeder.php
+++ database/seeds/OperationSeeder.php
@@ -14,11 +14,13 @@ class OperationSeeder extends Seeder
         $operation = [
             'login', 'forgetpassword',
             'register', 'get-members-list', 'get-member-detail', 'update-member-detail', 'delete-member',
+            'create-systemuser', 'get-systemusers-list', 'get-systemuser-detail', 'update-systemuser-detail', 'delete-systemuser',
             'post-article', 'get-article-list', 'get-article-detail', 'update-article-detail', 'delete-article'
         ];
         $operationZh = [
             '登入', '忘記密碼',
             '註冊', '獲取會員列表', '獲取會員詳細資訊', '修改會員詳細資料', '刪除會員',
+            '新增後台使用者', '獲取後台使用者列表', '獲取後台使用者詳細資訊', '修改後台使用者詳細資料', '刪除後台使用者',
             '新增文章', '獲取文章列表', '獲取文章詳細資訊', '更新文章詳細資料', '刪除文章'
         ];
         for ($i = 0; $i < sizeof($operation); $i++) {
