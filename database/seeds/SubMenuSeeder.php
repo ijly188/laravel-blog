e1bb6ab6be851815d40a9cd434bbdeb4570de130
terryyao <terry.yao.global@gmail.com>
2020-08-05 17:43:32 +0800

    create crud list detail about systemuser

--- database/seeds/SubMenuSeeder.php
+++ database/seeds/SubMenuSeeder.php
@@ -17,15 +17,18 @@ class SubMenuSeeder extends Seeder
         $name = [
             '會員列表', '會員資料', '新增會員', '修改會員', '刪除會員',
             '文章列表', '文章資料', '新增文章', '修改文章', '刪除文章',
+            '系統使用者列表', '系統使用者資料', '新增系統使用者', '修改系統使用者', '刪除系統使用者',
         ];
 
         $main_menu_id = [
             '1', '1', '1', '1', '1',
             '2', '2', '2', '2', '2',
+            '3', '3', '3', '3', '3',
         ];
         $route = [
             '/member-list', '/member-detail', '/create-member', '/update-member', '/delete-member',
             '/article-list', '/article-detail', '/create-article', '/update-article', '/delete-article',
+            '/systemuser-list', '/systemuser-detail', '/create-systemuser', '/update-systemuser', '/delete-systemuser',
         ];
         for ($i = 0; $i < sizeof($name); $i++) {
             $randomOperation = $faker->randomElements($operation->pluck('id'), mt_rand() % $operation->count() + 1);
